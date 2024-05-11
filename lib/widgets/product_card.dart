64111f17b0c3f94e17990a1657e998c9bc370ee0
Abhay Rathore <abhayrathore036@gmail.com>
2024-05-11 21:20:36 +0530

    update

---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class ProductCard extends StatelessWidget {
+  final String title;
+  final double price;
+  final String image;
+  final Color backgroundColor;
+  const ProductCard({
+    Key? key,
+    required this.title,
+    required this.price,
+    required this.image,
+    required this.backgroundColor,
+  }): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(20),
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: backgroundColor,
+        borderRadius: BorderRadius.circular(20),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            title,
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+          const SizedBox(height: 5),
+          Text(
+            '\$$price',
+            style: Theme.of(context).textTheme.bodySmall,
+          ),
+          const SizedBox(height: 5),
+          Center(
+            child: image != null
+                ? Image.asset(image!, height: 175) // Use null-aware operator to access 'image'
+                : const SizedBox(), // Show an empty SizedBox if image is null
+          ),
+
+        ],
+      ),
+    );
+  }
+}
